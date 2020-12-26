388b58d529e0d5c8b337fd70623447ba78770954
Hazim Arafa <hazimarafa80@gmail.com>
2020-12-25 17:55:33 -0800

    added a pricing page
     im gonna do the home page later because i dont know what to do

---
+++ src/Components/Pricing.tsx
@@ -0,0 +1,18 @@
+export default function Pricing() {
+    return (
+        <div className='price'>
+            <h1>About Hazimos</h1>
+            <p>Wild Species driven to extinction throught inability to mate.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate suscipit, in mollitia voluptate ducimus itaque ratione. Natus culpa tempora beatae autem, quaerat saepe suscipit voluptate sed inventore minima molestias pariatur.</p>
+        
+            <h1>More About Something</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate suscipit, in mollitia voluptate ducimus itaque ratione. Natus culpa tempora beatae autem, quaerat saepe suscipit voluptate sed inventore minima molestias pariatur.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate suscipit, in mollitia voluptate ducimus itaque ratione. Natus culpa tempora beatae autem, quaerat saepe suscipit voluptate sed inventore minima molestias pariatur.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate suscipit, in mollitia voluptate ducimus itaque ratione. Natus culpa tempora beatae autem, quaerat saepe suscipit voluptate sed inventore minima molestias pariatur.</p>
+
+        </div>
+    )
+}
