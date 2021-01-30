cb50ba30aec20a7d45bccd1318fb5b92b3b8f056
Hazim Arafa <hazimarafa80@gmail.com>
2021-01-29 22:15:52 -0800

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,5 +3,7 @@ import styled from "styled-components";
 const Footer = styled.footer``;
 // eslint-disable-next-line import/no-default-export
 export default function Main(): JSX.Element {
-	return <Footer />;
+	return (
+		<Footer />;
+	);
 }
