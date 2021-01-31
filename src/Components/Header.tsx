3dc57072d3226929c10e9f386876e4dfe782e1a2
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-31 10:37:04 -0600

    You Dumbass

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,47 @@
+import Link from 'next/link'
 import styled from "styled-components";
 
-const Header = styled.header``;
+const Header = styled.header`
+	display: flex;
+	justify-content: space-between;
+	box-sizing: border-box;
+	padding: 5px 5%;
+	align-items:center;
+`;
+const StyledUl = styled.ul`
+	display:flex;
+`
+const StyledLink = styled.li`
+	margin:0 20px;
+	font-size: 22px;
+	text-transform: uppercase;
+`
+type a = {
+	title: string;
+	link: string;
+};
+
+const headerStuff: a[] = [
+	{ title: "Home", link: "/#" },
+	{ title: "home", link: "/#" },
+	{ title: "home", link: "/#" },
+	{ title: "Contact", link: "/#" },
+];
+
 // eslint-disable-next-line import/no-default-export
-export default function header(): JSX.Element {
-	return <Header />;
+export default function Main(): JSX.Element {
+	return (
+		<Header>
+			<h1>Elegant Themes</h1>
+			<StyledUl>
+				{headerStuff.map(data => 
+					<StyledLink key={data.title}>
+						<Link href={data.link}>
+							<a>{data.title }</a>
+						</Link>
+					</StyledLink>
+				)}
+			</StyledUl>
+		</Header>
+	);
 }
